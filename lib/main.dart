b22e4c355f5d936bad0eccadcbacb445c2b46cef
kubwimanav <kubwimanavincent2020@gmail.com>
2024-09-30 22:48:50 +0200

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -6,15 +6,37 @@ void main() => runApp(MaterialApp(
           title: Text('my first app'),
           centerTitle: true,
         ),
-        body: Center(
-            child: Image.asset('vincent.jpg')
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Container(
+              child: Text(
+                'hello every one hope your doing well',
+                style: TextStyle(
+                    color: Colors.blue,
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold),
+              ),
             ),
+            Container(
+              child: Image.asset('lib/assets/vincent.jpg'),
+            ),
+         FloatingActionButton(onPressed: (){
+          
+         },
+         
+         child: Text('data'),
+         ),
+                    
+          ],
+        ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {},
           child: Text(
             'clicke me',
             style: TextStyle(color: Colors.amber),
           ),
+          
           backgroundColor: Colors.blue,
         ),
       ),
