2759923960819c4faa6f215385ad88f9dfb55cd0
kubwimanav <kubwimanavincent2020@gmail.com>
2024-09-30 20:19:24 +0200

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,21 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-      home: Home(),
-    ));
-
-class Home extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('my first app'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text(
-          'hello every one',
-          style: TextStyle(
-              fontSize: 20.0,
-              fontWeight: FontWeight.bold,
-              color: Colors.amber,
-              letterSpacing: 1.0,
-              fontFamily: 'IndieFlower'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('my first app'),
+          centerTitle: true,
+        ),
+        body: Center(
+            child: Image.asset('vincent.jpg')
+            ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Text(
+            'clicke me',
+            style: TextStyle(color: Colors.amber),
+          ),
+          backgroundColor: Colors.blue,
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Text('clickme'),
-      ),
-    );
-  }
-}
+    ));
